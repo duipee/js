948e8bd088a6e63be2fde36c67a89a138f96d72e
duipee <42163278+duipee@users.noreply.github.com>
2019-07-30 16:58:35 +0700

    Add files via upload

---
+++ compress-html-v6c.js
@@ -0,0 +1,46 @@
+document.getElementById("HTMLcompressor").addEventListener("click", function () {
+	var allHTML = document.getElementById("oldCode").value;
+	var headHTML = "";
+	var removeThis = "";
+	var headstatus = document.getElementById("headstatus").checked;
+	if(headstatus != true){
+		//Compress all the things!
+		allHTML = allHTML.replace(/(\r\n|\n|\r|\t)/gm,"");
+		allHTML = allHTML.replace(/\s+/g," ");
+	}else{
+		//Don't compress the head
+		allHTML = allHTML.replace(new RegExp("</HEAD","gi"),'</head');
+		allHTML = allHTML.replace(new RegExp("</head ","gi"),'</head');
+		
+		var bodySplit = "</head>"; 
+		var i = allHTML.indexOf(bodySplit) != -1;
+		if(i == true){
+			var bodySplit = "</head>"; 
+			tempo = allHTML.split(new RegExp(bodySplit,'i'));
+			headHTML = tempo[0];
+			allHTML = tempo[1];
+		}else{
+			bodySplit = ""; 
+		}
+		allHTML = allHTML.replace(/(\r\n|\n|\r|\t)/gm,"");
+		allHTML = allHTML.replace(/\s+/g," ");
+		allHTML = headHTML + bodySplit + '\n' + allHTML;
+	}
+	document.getElementById("newCode").value = allHTML;
+});	
+
+//reset text areas
+document.getElementById("clearText").addEventListener("click", function () {
+	document.getElementById("oldCode").value = "";
+	document.getElementById("newCode").value = "";
+	document.getElementById("oldCode").focus();
+});
+
+var textBox = document.getElementById("newCode");
+textBox.onfocus = function() {
+	textBox.select();
+	textBox.onmouseup = function() {
+		textBox.onmouseup = null;
+		return false;
+	};
+};	
